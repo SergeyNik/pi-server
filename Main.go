bbfd993239e65615d8a8c3801d52adc7d1b33905
snikolchikov <snikolchikov@navicons.ru>
2020-06-24 20:48:13 +0300

    Add gron

--- Main.go
+++ Main.go
@@ -2,33 +2,44 @@ package main
 
 import (
 	"fmt"
+	"github.com/roylee0704/gron"
 	"github.com/ssimunic/gosensors"
+	"net/http"
 	"time"
 )
 
 func main() {
-	//_ = os.Mkdir("subdir", 0755) create folder
-	initSensorsInfo()
+	task := gron.New()
+
+	http.HandleFunc("/start", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			task.AddFunc(gron.Every(2*time.Second), getSensorsInfo)
+			task.Start()
+			fmt.Fprintf(w, "Task started!")
+		}
+	})
+
+	http.HandleFunc("/stop", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			task.Stop()
+			fmt.Fprintf(w, "Task stopped!")
+		}
+	})
+
+	http.ListenAndServe(":3000", nil)
 }
 
-func initSensorsInfo() {
+func getSensorsInfo() {
 	sensors, err := gosensors.NewFromSystem()
-
 	if err != nil {
 		panic(err)
 	}
-
-	for true {
-		time.Sleep(time.Second * 2)
-		fmt.Println(sensors)
-		//for chip := range sensors.Chips {
-			// Iterate over entries
-			//for key, value := range sensors.Chips[chip] {
-				// If CPU or GPU, print out
-				//if key == "Core 0" || key == "GPU" {
-				//	fmt.Println(key, value)
-				//}
-			//}
-		//}
+	//fmt.Println(sensors)
+	for chip := range sensors.Chips {
+		for key, value := range sensors.Chips[chip] {
+			if key == "Core 0" || key == "GPU" {
+				fmt.Println(key, value)
+			}
+		}
 	}
 }
