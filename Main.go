b1663b9eb14d7583e02f74fae94af27c21c77b05
snikolchikov <snikolchikov@navicons.ru>
2020-07-02 09:15:32 +0300

    add temp by request

--- Main.go
+++ Main.go
@@ -2,83 +2,25 @@ package main
 
 import (
 	"fmt"
-	//"github.com/roylee0704/gron"
-	//"github.com/ssimunic/gosensors"
-	//"net/http"
-	//"time"
+	"net/http"
 	"os/exec"
 )
 
-//
-//func main() {
-//	task := gron.New()
-//	taskPi := gron.New()
-//
-//	http.HandleFunc("/start", func(w http.ResponseWriter, r *http.Request) {
-//		if r.Method == http.MethodGet {
-//			task.AddFunc(gron.Every(2*time.Second), getSensorsInfoLm)
-//			task.Start()
-//			fmt.Fprintf(w, "Task started!")
-//		}
-//	})
-//
-//	http.HandleFunc("/stop", func(w http.ResponseWriter, r *http.Request) {
-//		if r.Method == http.MethodGet {
-//			task.Stop()
-//			fmt.Fprintf(w, "Task stopped!")
-//		}
-//	})
-//
-//	http.HandleFunc("/pi/start", func(w http.ResponseWriter, r *http.Request) {
-//		if r.Method == http.MethodGet {
-//			taskPi.AddFunc(gron.Every(2*time.Second), getSensorsInfoPi)
-//			taskPi.Start()
-//			fmt.Fprintf(w, "Task started!")
-//		}
-//	})
-//
-//	http.HandleFunc("/pi/stop", func(w http.ResponseWriter, r *http.Request) {
-//		if r.Method == http.MethodGet {
-//			taskPi.Stop()
-//			fmt.Fprintf(w, "Task stopped!")
-//		}
-//	})
-//
-//	http.HandleFunc("/pi/temp", func(w http.ResponseWriter, r *http.Request) {
-//		if r.Method == http.MethodGet {
-//			fmt.Fprintf(w, currentTempPi())
-//		}
-//	})
-//
-//	http.ListenAndServe(":3000", nil)
-//}
-//
-//func getSensorsInfoLm() {
-//	sensors, err := gosensors.NewFromSystem()
-//	if err != nil {
-//		panic(err)
-//	}
-//	fmt.Println(sensors)
-//}
-//
-//func getSensorsInfoPi() {
-//	command := exec.Command("vcgencmd", "measure_temp")
-//	if output, error := command.Output(); error != nil {
-//		fmt.Println("Error: ", error)
-//	} else {
-//		fmt.Println("CPU temp: ", output)
-//	}
-//}
-
 func main() {
-	currentTempPi()
+	http.HandleFunc("/temp", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			_, _ = fmt.Fprintln(w, getSensorsInfoPi())
+		}
+	})
+
+	_ = http.ListenAndServe(":3000", nil)
 }
 
-func currentTempPi() {
+func getSensorsInfoPi() string {
 	command := exec.Command("vcgencmd", "measure_temp")
-	if output, error := command.Output(); error != nil {
-		fmt.Println("Error: ", error)
-	} else {
-		fmt.Println(string(output))
+	output, err := command.Output()
+	if err != nil {
+		panic(err)
 	}
+	return string(output)
 }
